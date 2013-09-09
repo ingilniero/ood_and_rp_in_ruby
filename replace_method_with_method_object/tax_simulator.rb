48c67495220e2c68fe6b75d725b35fe54eae6895
Gilberto Villa <ingilniero@gmail.com>
2013-09-08 19:55:07 -0500

    Move logic to a new object with state

--- replace_method_with_method_object/tax_simulator.rb
+++ replace_method_with_method_object/tax_simulator.rb
@@ -4,23 +4,36 @@ class TaxSimulator
   end
 
   def simulate_return(income: nil, expenses: 0, type: :dependent_worker)
-    return_value = 0
-    number_of_people_under_roof = 1
+    TaxAlgorihm.new(income, expenses, type).simulate
+  end
+end
 
-    if type == :dependent_worker
-      return_value += income * 0.02
+class TaxAlgorihm
+  def initialize(income, expenses, type)
+    @income = income
+    @expenses = expenses
+    @type = type
+
+    @return_value = 0
+    @number_of_people_under_roof = 1
+  end
+
+  def simulate
+
+    if @type == :dependent_worker
+      @return_value += @income * 0.02
     else
-      return_value += income * 0.04
+      @return_value += @income * 0.04
     end
 
-    if number_of_people_under_roof > 2
-      return_value *= 1.10
+    if @number_of_people_under_roof > 2
+      @return_value *= 1.10
     end
 
-    if income - expenses > income * 0.5
-      return_value += expenses * 0.05
+    if @income - @expenses > @income * 0.5
+      @return_value += @expenses * 0.05
     end
 
-    return_value -= expenses * 0.30
+    @return_value -= @expenses * 0.30
   end
 end

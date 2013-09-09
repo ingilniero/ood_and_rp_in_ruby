ee1fd7df545ba722241e49f9f018ecf195e994f4
Gilberto Villa <ingilniero@gmail.com>
2013-09-08 19:33:52 -0500

    Improve the way we define our products

--- replace_array_with_object/cart.rb
+++ replace_array_with_object/cart.rb
@@ -2,10 +2,20 @@ class Cart
   attr_reader :products
 
   def initialize(products)
-    @products = products
+    @products = products.map { |product| Product.new *product }
   end
 
   def total
-    products.inject(0) { |sum, product| sum + product[2] }
+    products.inject(0) { |sum, product| sum + product.price }
+  end
+end
+
+class Product
+  attr_reader :price
+
+  def initialize(name, color, price)
+    @name = name
+    @color = color
+    @price = price
   end
 end
